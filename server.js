b0b490d4a5bcf51a2d3e28ba2d6fedf15420f9b3
mfGlurakdltn <mfglurak@gmail.com>
2026-02-06 21:32:58 +0100

    Fix CORS: hardcode wildcard origin

--- server.js
+++ server.js
@@ -16,8 +16,7 @@ const supabase = createClient(
 
 // CORS
 app.use((req, res, next) => {
-  const allowed = process.env.ALLOWED_ORIGIN || "*";
-  res.header("Access-Control-Allow-Origin", allowed);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.sendStatus(200);
